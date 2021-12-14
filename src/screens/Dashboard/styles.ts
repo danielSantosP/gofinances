34e841fbcb924ea213926e02a7acc9f1f2710ca5
Daniel Santos do Patrocinio <daniel.santos@ceresinvestimentos.com>
2021-12-14 12:54:07 -0300

    feat: create global theme -  colors and fonts

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -2,13 +2,5 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
-  background-color: #999;
-`;
-
-export const Title = styled.Text`
-font-size: 24px;
-font-weight: bold;
-color: purple;
+  background-color: ${({theme}) => theme.colors.background};
 `;
