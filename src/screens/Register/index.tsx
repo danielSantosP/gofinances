d9ed6de3f49cc9b303f3a820f0dd3ca1c25d049b
Daniel Santos do Patrocinio <daniel.santos@ceresinvestimentos.com>
2021-12-15 16:25:00 -0300

    feat: transaction type options

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from '../../components/Form/Button';
 import { Input } from '../../components/Form/Input';
+import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
 import { 
   Container,
   Header,
   Title,
   Form,
   Fields,
+  TransactionTypes,
 } from './styles';
 
 export function Register(){
+  const [transactionType, setTransactionType] = useState('')
+
+  function handleTransactionTypeSelect(type: 'up' | 'down'){
+    setTransactionType(type)
+  }
+
   return (
     <Container>
       <Header>
@@ -19,6 +27,20 @@ export function Register(){
         <Fields>
           <Input placeholder="Nome" />
           <Input placeholder="Preço" />
+          <TransactionTypes>
+            <TransactionTypeButton
+              type="up"
+              title="Income"
+              isActive={transactionType === 'up'}
+              onPress={() => handleTransactionTypeSelect('up')}
+            />
+            <TransactionTypeButton
+              type="down"
+              title="Outcome"
+              isActive={transactionType === 'down'}
+              onPress={() => handleTransactionTypeSelect('down')}
+            />
+          </TransactionTypes>
         </Fields>
         <Button title="Enviar" />
       </Form>
