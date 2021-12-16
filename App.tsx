e106aa452b1890719ea5a92e5b24751abac643c3
Daniel Santos do Patrocinio <daniel.santos@ceresinvestimentos.com>
2021-12-16 16:27:30 -0300

    feat: navigation

--- App.tsx
+++ App.tsx
@@ -8,7 +8,8 @@ import {
   Poppins_700Bold
 } from '@expo-google-fonts/poppins';
 import theme from './src/global/styles/theme';
-import { Register } from './src/screens/Register';
+import { NavigationContainer } from '@react-navigation/native'
+import { AppRoutes } from './src/routes/app.routes'
 
 export default function App() {
 
@@ -21,7 +22,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   )
 }
