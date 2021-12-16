3a4d7ca3fc0acf6bcca2b795e4fd2220942d0979
Daniel Santos do Patrocinio <daniel.santos@ceresinvestimentos.com>
2021-12-16 00:00:09 -0300

    feat: finish layout category select component

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
+import { Modal } from 'react-native';
 import { Button } from '../../components/Form/Button';
+import { CategorySelectButton } from '../../components/Form/CategorySelectButton';
 import { Input } from '../../components/Form/Input';
 import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
+import { CategorySelect } from '../CategorySelect';
 import { 
   Container,
   Header,
@@ -13,9 +16,22 @@ import {
 
 export function Register(){
   const [transactionType, setTransactionType] = useState('')
+  const [categoryModalOpen, setCategoryModalOpen] = useState(false)
+  const [category, setCategory] = useState({
+    key: 'category',
+    name: 'Categoria',
+  })
 
   function handleTransactionTypeSelect(type: 'up' | 'down'){
-    setTransactionType(type)
+    setTransactionType(type);
+  }
+  
+  function handleOpenSelectCategoryModal(){
+    setCategoryModalOpen(true);
+  }
+
+  function handleCloseSelectCategoryModal(){
+    setCategoryModalOpen(false);
   }
 
   return (
@@ -41,9 +57,20 @@ export function Register(){
               onPress={() => handleTransactionTypeSelect('down')}
             />
           </TransactionTypes>
+          <CategorySelectButton
+            title={category.name} 
+            onPress={handleOpenSelectCategoryModal}
+          />
         </Fields>
         <Button title="Enviar" />
       </Form>
+      <Modal visible={categoryModalOpen}>
+        <CategorySelect
+          category={category}
+          setCategory={setCategory}
+          closeSelectCategory={handleCloseSelectCategoryModal}
+        />
+      </Modal>
     </Container>
   );
 }
