eee18f05a13eef85bef24f13641adb0ebf2a86d0
Daniel Santos do Patrocinio <daniel.santos@ceresinvestimentos.com>
2021-12-17 10:57:10 -0300

    feat: add asyncSstorage and start transaction registration

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Modal, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
 import * as Yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import { useForm } from 'react-hook-form';
 import { Button } from '../../components/Form/Button';
 import { CategorySelectButton } from '../../components/Form/CategorySelectButton';
@@ -34,6 +35,7 @@ const schema = Yup.object().shape({
 export function Register(){
   const [transactionType, setTransactionType] = useState('')
   const [categoryModalOpen, setCategoryModalOpen] = useState(false)
+  const dataKey = '@gofinances:transactions';
   const [category, setCategory] = useState({
     key: 'category',
     name: 'Categoria',
@@ -54,7 +56,7 @@ export function Register(){
     setCategoryModalOpen(false);
   }
   
-  function handleRegister(form: FormData){
+  async function handleRegister(form: FormData){
     if(!transactionType)
       return Alert.alert('Selecione o tipo da transação')
     if(category.key === 'category')
@@ -66,8 +68,22 @@ export function Register(){
       transactionType,
       category: category.key,
     }
-    console.log(data)
+
+    try {
+      await AsyncStorage.setItem(dataKey, JSON.stringify(data));
+    } catch (error) {
+      console.log(error)
+      Alert.alert("Não foi possível salvar");   
+    }
   }
+
+  useEffect(() => {
+    async function loadData(){
+      const data = await AsyncStorage.getItem(dataKey)
+      console.log(JSON.parse(data!))
+    }
+    loadData();
+  }, [])
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
