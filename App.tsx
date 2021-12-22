afe8f065d932dbbde1b7c006703455d63422c516
Daniel Santos do Patrocinio <daniel.santos@ceresinvestimentos.com>
2021-12-22 15:54:20 -0300

    wip: start of signin screen

--- App.tsx
+++ App.tsx
@@ -12,8 +12,8 @@ import {
 } from '@expo-google-fonts/poppins';
 import theme from './src/global/styles/theme';
 import { NavigationContainer } from '@react-navigation/native'
-import { AppRoutes } from './src/routes/app.routes'
-
+// import { AppRoutes } from './src/routes/app.routes'
+import { SignIn } from './src/screens/SignIn'
 export default function App() {
 
   const [fontsLoaded] = useFonts({
@@ -27,7 +27,7 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <NavigationContainer>
         <StatusBar barStyle="light-content" />
-        <AppRoutes />
+        <SignIn />
       </NavigationContainer>
     </ThemeProvider>
   )
