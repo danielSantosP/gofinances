325b4cede196d53d57f04f2056693bacd67deed4
Daniel Santos do Patrocinio <daniel.santos@ceresinvestimentos.com>
2021-12-23 13:55:31 -0300

    feat: signin with google

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,4 +1,5 @@
-import React, { useContext } from 'react';
+import React from 'react';
+import { Alert } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import AppleSvg from '../../assets/apple.svg';
 import GoogleSvg from '../../assets/google.svg';
@@ -16,8 +17,19 @@ import {
 } from './styles';
 
 export function SignIn(){
-  const {user } = useAuth()
-  console.log(user.id);
+  const { user, signInWithGoogle } = useAuth()
+  
+  async function handleSignInWithGoogle(){
+    try {
+      await signInWithGoogle();
+      console.log(user)
+
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Não foi possível conectar a conta Google');
+    }
+  }
+
   return (
     <Container>
       <Header>
@@ -35,6 +47,7 @@ export function SignIn(){
           <SignInSocialButton 
             title='Entrar com Google'
             svg={GoogleSvg}
+            onPress={handleSignInWithGoogle}
           />
           <SignInSocialButton 
             title='Entrar com Apple'
